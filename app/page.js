d077b1f923a82429e6104300c9f9ecb9f2ba86f7
Miskat Alam <misky0310@Miskats-Laptop.local>
2024-06-22 11:19:36 +0530

    Initial Commit

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-full bg-green-500 h-screen flex justify-center items-center">
+    <div className="w-full bg-green-900 h-screen flex justify-center items-center">
       Hi
     </div>
   );
