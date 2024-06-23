ea12040d792c0b6a722d73664756501a634c6a4b
Miskat Alam <misky0310@Miskats-Laptop.local>
2024-06-23 10:19:35 +0530

    Hero section added

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-full bg-green-900 text-white h-screen flex justify-center items-center">
-      Hi
-    </div>
+    <>
+      <Hero/>
+    </>
   );
 }
