a5ff9622f235dc12659ce20cc50818dd6cd4668d
Miskat Alam <misky0310@Miskats-Laptop.local>
2024-06-23 10:43:10 +0530

    Footer added

---
+++ components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image from 'next/image'
+import logo from '@/assets/images/logo.png'
+
+const Footer = () => {
+    const curentYear = new Date().getFullYear()
+  return (
+    <footer className="bg-gray-200 py-4 absolute bottom-0 w-full">
+      <div
+        className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4"
+      >
+        <div className="mb-4 md:mb-0">
+          <Image src={logo} alt="Logo" className="h-8 w-auto" />
+        </div>
+        <div>
+          <p className="text-sm text-gray-500 mt-2 md:mt-0">
+            &copy; {curentYear} PropertyPulse. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
