28e024c33b7fa799c4b7b5f5de63b04605196e76
Miskat Alam <misky0310@Miskats-Laptop.local>
2024-07-10 15:30:31 +0530

    Added the provison to call database

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 import Hero from "@/components/Hero";
 import HomeProperties from "@/components/HomeProperties";
 import InfoBoxes from "@/components/InfoBoxes";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
